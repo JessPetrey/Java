9169ad44a26a0949be61b2a51e2afa11c04a48d7
Jessica Petrey <jesipetrey@gmail.com>
2023-03-05 14:19:53 -0800

    basic code from platform

---
+++ fundamentals/AlfredBot/AlfredQuotes.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class AlfredQuotes {
+    public String basicGreeting() {
+        return "Hello, lovely to see you. How are you?";
+    }
+
+    public String guestGreeting(String name) {
+        return "placeholder"
+    }
+
+    public String dateAnnouncement() {
+        return "placeholder"
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+        return "placeholder"
+    }
+}
