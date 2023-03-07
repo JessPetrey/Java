af120289a157318e3454c43fd080accc7f08d639
Jessica Petrey <jesipetrey@gmail.com>
2023-03-07 12:35:02 -0800

    init commit for barista challenge-copy last asgnmt

---
+++ javaOOP/baristaChallenge/Order.java
@@ -0,0 +1,13 @@
+//  order class should have string name, double total,
+// boolean ready, and arraylist of items class
+import java.util.ArrayList;
+
+
+public class Order {
+    public String name;
+    public double total = 0;
+    public boolean ready = false;
+    public ArrayList<Item> items = new ArrayList<Item>();
+
+
+}
